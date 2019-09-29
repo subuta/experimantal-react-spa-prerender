7d5892b5cba8dfe25cfeb3ec56aeb026a37c4f81
subuta <shunta.3110@gmail.com>
2019-09-29 22:25:19 +0900

    Add initial files.

---
+++ example/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const main = async () => {
+  ReactDOM.render(
+    <h1>hello!</h1>,
+    document.getElementById('app')
+  )
+}
+
+main()
